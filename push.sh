8ae359e6884b6d3a802a36303f8f7e1c9b76461f
forkjoseph <forkjoseph@gmail.com>
2015-07-22 14:19:49 -0400

    adjusted

--- push.sh
+++ push.sh
@@ -1,9 +1,12 @@
-if [ "init" = $1 ]
+if [[ "init" = $1 ]];
 then
 	adb shell mount -o rw,remount /
 	adb push test.sh /
 	adb shell chmod 777 /test.sh
 	adb shell mount -o rw,remount /system
-fi 
+	adb push joseph sdcard/
+	adb shell ./test.sh
+	adb shell chmod 777 /system/bin/joseph
+fi
 adb push ../../out/target/product/hammerhead/system/bin/joseph sdcard/
-
+adb shell ./test.sh
