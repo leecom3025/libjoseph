2a1e4850620ebfb1cffb25416dc2df9d2830c0aa
forkjoseph <forkjoseph@gmail.com>
2015-10-09 19:27:50 -0400

    dynamic time

--- src/monitor.c
+++ src/monitor.c
@@ -6,8 +6,8 @@
 #endif
 
 #ifdef LOG_TAG
-#undef LOG_TAG
-#endif 
+#undef LOG_TAG 
+#endif
 
 #define LOG_TAG "TORH"
 
@@ -18,7 +18,19 @@ int main (int argc, char *argv[]) {
 		int a = 1;
 	}
 	int period = 100000; // 100 mili == 0.1 sec
-	char st[2048];
+  int c; 
+
+  while ((c = getopt(argc, argv, "t:")) != -1) {
+    switch(c) {
+      case 't':
+        period =  atoi(optarg);
+        break;
+      default:
+        printf("help not supported\n");
+    }
+  }
+	
+  char st[2048];
 	struct jcpu **cpu;
 	Joseph_CPU_ops(&cpu);
 
@@ -29,28 +41,41 @@ int main (int argc, char *argv[]) {
 
 #if _PRODUCT == _hima
       sprintf(st, "%lu, %d,%d,%d,%d,%d,%d,%d,%d, %d,%d,%d,%d,%d,%d,%d,%d\n", getmicro(),
-				cpu[0]->temp, cpu[1]->temp, cpu[2]->temp, cpu[3]->temp, 
-				cpu[4]->temp, cpu[5]->temp, cpu[6]->temp, cpu[7]->temp, 
-				cpu[0]->freq, cpu[1]->freq, cpu[2]->freq, cpu[3]->freq,
-				cpu[4]->freq, cpu[5]->freq, cpu[6]->freq, cpu[7]->freq);
+				cpu[0]->temp, cpu[1]->temp, 
+        cpu[2]->temp, cpu[3]->temp,
+				cpu[4]->temp, cpu[5]->temp, 
+        cpu[6]->temp, cpu[7]->temp,
+				cpu[0]->freq, cpu[1]->freq, 
+        cpu[2]->freq, cpu[3]->freq,
+				cpu[4]->freq, cpu[5]->freq, 
+        cpu[6]->freq, cpu[7]->freq);
           
-/*			sprintf(st, "%lu, %d, %d, %d, %d, %d, %d, %d, %d, %d,%d, %d,%d, %d,%d, %d,%d, %d,%d, %d,%d, %d,%d, %d,%d\n",
+/* sprintf(st, "%lu, %d, %d, %d, %d, %d, %d, %d, %d, %d,%d, %d,%d, 
+%d,%d, %d,%d, %d,%d, %d,%d, %d,%d, %d,%d\n",
 				getmicro(),
-				cpu[0]->temp, cpu[1]->temp, cpu[2]->temp, cpu[3]->temp, 
-				cpu[4]->temp, cpu[5]->temp, cpu[6]->temp, cpu[7]->temp, 
-				cpu[0]->util, cpu[0]->freq, cpu[1]->util, cpu[1]->freq,
-				cpu[2]->util, cpu[2]->freq, cpu[3]->util, cpu[3]->freq,
-				cpu[4]->util, cpu[4]->freq, cpu[5]->util, cpu[5]->freq,
-				cpu[6]->util, cpu[6]->freq, cpu[7]->util, cpu[7]->freq);
-*/
+				cpu[0]->temp, cpu[1]->temp, 
+cpu[2]->temp, cpu[3]->temp,
+				cpu[4]->temp, cpu[5]->temp, 
+cpu[6]->temp, cpu[7]->temp,
+				cpu[0]->util, cpu[0]->freq, 
+cpu[1]->util, cpu[1]->freq,
+				cpu[2]->util, cpu[2]->freq, 
+cpu[3]->util, cpu[3]->freq,
+				cpu[4]->util, cpu[4]->freq, 
+cpu[5]->util, cpu[5]->freq,
+				cpu[6]->util, cpu[6]->freq, 
+cpu[7]->util, cpu[7]->freq); */
 #else
       sprintf(st, "%lu, %d,%d,%d,%d, %d,%d,%d,%d\n",
 				getmicro(),
-				cpu[0]->temp, cpu[1]->temp, cpu[2]->temp, cpu[3]->temp, 
-				cpu[0]->freq, cpu[1]->freq, cpu[2]->freq, cpu[3]->freq);
+				cpu[0]->temp, cpu[1]->temp, 
+        cpu[2]->temp, cpu[3]->temp,
+				cpu[0]->freq, cpu[1]->freq, 
+        cpu[2]->freq, cpu[3]->freq);
 #endif
       ALOGD("%s", st);
 		}
 	}
 	return 0;
 }
+
